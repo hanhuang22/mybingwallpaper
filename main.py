1c28bc8f1abdb5dfa59902eefef07e9cfa7973db
huanghan <han.huang@cripac.ia.ac.cn>
2025-04-10 03:01:18 +0800

    update bing api

--- main.py
+++ main.py
@@ -13,27 +13,30 @@ def get_now_time():
 
 
 def main(run_type):
-    data = requests.get(f"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt={run_type}").json()
+    # data = requests.get(f"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt={run_type}").json()
+    data = requests.get(f"https://www.bing.com/hp/api/model?mkt={run_type}").json()
     print(f"[{get_now_time()}] 开始读取 API")
-    data_list = data["images"]
+    data_list = data["MediaContents"]
     print(f"[{get_now_time()}] 获取到 {len(data_list)} 条数据")
     
     # Process each image in the data list
-    for image in data_list:
+    for image_data in data_list:
         # Extract required values
-        enddate = image.get('enddate', '')
+        ssd = image_data.get('Ssd', '')
+        enddate = str(int(ssd[:8])+1)
+        image_data = image_data.get('ImageContent', '')
         date = enddate[:4] + '-' + enddate[4:6] + '-' + enddate[6:8]
-        title = image.get('title', '')
-        copyright_text = image.get('copyright', '')
-        imgtitle = f"{title}  |  {copyright_text}  -  {enddate[:4] + '/' + enddate[4:6] + '/' + enddate[6:8]}"
+        headline = image_data.get('Headline', '')
+        title = image_data.get('Title', '')
+        copyright_text = image_data.get('Copyright', '')
+        imgtitle = f"{headline}  |  {title} {copyright_text}  -  {enddate[:4] + '/' + enddate[4:6] + '/' + enddate[6:8]}"
         
         # Format the image URL to UHD.jpg
-        urlbase = image.get('urlbase', '')
+        urlbase = image_data.get('Image', '').get('Url', '').split('_')[:2]
+        urlbase = '_'.join(urlbase)
         imgurl = f"https://cn.bing.com{urlbase}_UHD.jpg"
         
-        # Create a default description based on the image title
-        # This can be updated later with more detailed information
-        imgdesc = f"这是一张{title}的美丽图片，展示了{copyright_text}。这些图片由微软必应每日更新，为用户提供高质量的壁纸。"
+        imgdesc = image_data.get('Description', '')
         
         # Create the output directory if it doesn't exist
         os.makedirs('date', exist_ok=True)
